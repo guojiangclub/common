de93cd5bde9adbae661b3f3cbb57f49017baf016
zidaibudai <coolchen.hao@gmail.com>
2019-03-07 15:02:53 +0800

    add some help functions: is_mobile,is_mail,is_username etc.

--- src/helpers.php
+++ src/helpers.php
@@ -12,3 +12,54 @@ if (!function_exists('platform_application')) {
         return app('ibrand.platform');
     }
 }
+
+if (!function_exists('is_mobile')) {
+
+    /**
+     * Check string is a mobile number.
+     *
+     * @param string $mobile
+     * @param string $type : 'china' or 'international'
+     * @return bool|string
+     */
+    function is_mobile(string $mobile, string $type = 'china')
+    {
+        if ($type == 'china') {
+            $regExp = '/^(?:\+?86)?1(?:3\d{3}|5[^4\D]\d{2}|8\d{3}|7(?:[01356789]\d{2}|4(?:0\d|1[0-2]|9\d))|9[189]\d{2}|6[567]\d{2}|4[579]\d{2})\d{6}$/';
+        } else {
+            $regExp = '/^(\\+\\d{2}-)?(\\d{2,3}-)?([1][3,4,5,7,8][0-9]\\d{8})$/';
+        }
+        return preg_match($regExp, $mobile) ? $mobile : false;
+    }
+}
+
+if (!function_exists('is_mail')) {
+
+    /**
+     * check string is a email
+     *
+     * @param string $email
+     * @return bool|string
+     */
+    function is_mail(string $email)
+    {
+        $regExp = '/^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/';
+        return preg_match($regExp, $email) ? $email : false;
+    }
+}
+
+if (!function_exists('is_username')) {
+
+
+    /**
+     * check string is a username
+     *
+     * @param string $username
+     * @return bool|string
+     */
+    function is_username(string $username)
+    {
+        $regExp = '/^[a-zA-Z\d\x{4e00}-\x{9fa5}]{2,20}$/u';
+        return preg_match($regExp, $username) ? $username : false;
+    }
+}
