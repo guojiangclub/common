205c7d7d3883dc266488ea92642660cb1e4d36c9
tangqi <tangqi_1987@sina.com>
2019-04-24 09:51:07 +0800

    添加获取用户unionid接口

--- src/Platform/Client.php
+++ src/Platform/Client.php
@@ -7,60 +7,67 @@ use Storage;
 
 /**
  * Class Client
+ *
  * @package iBrand\Common\Platform
  */
 class Client
 {
-    /**
-     * @var
-     */
-    protected $baseUri;
+	/**
+	 * @var
+	 */
+	protected $baseUri;
 
-    /**
-     * @var
-     */
-    protected $accessToken;
+	/**
+	 * @var
+	 */
+	protected $accessToken;
 
-    /**
-     * @var
-     */
-    protected $httpClient;
+	/**
+	 * @var
+	 */
+	protected $httpClient;
 
-    /**
-     * Client constructor.
-     * @param $baseUri
-     * @param $accessToken
-     */
-    public function __construct($baseUri, $accessToken)
-    {
-        $this->accessToken = $accessToken;
-        $this->baseUri = $baseUri;
-    }
+	/**
+	 * Client constructor.
+	 *
+	 * @param $baseUri
+	 * @param $accessToken
+	 */
+	public function __construct($baseUri, $accessToken)
+	{
+		$this->accessToken = $accessToken;
+		$this->baseUri     = $baseUri;
+	}
 
+	/**
+	 * get wechat oauth url from open platform.
+	 *
+	 * @param $redirectUri
+	 * @param $appid
+	 *
+	 * @return string
+	 */
+	public function getOauthUrl($redirectUri, $appid)
+	{
+		$query = http_build_query(['appid' => $appid, 'redirect' => $redirectUri]);
 
-    /**
-     * get wechat oauth url from open platform.
-     * @param $redirectUri
-     * @param $appid
-     * @return string
-     */
-    public function getOauthUrl($redirectUri, $appid)
-    {
-        $query = http_build_query(['appid' => $appid, 'redirect' => $redirectUri]);
-        return $this->baseUri . 'oauth?' . $query;
-    }
+		return $this->baseUri . 'oauth?' . $query;
+	}
 
-    /**
-     * get user.
-     * @param $appid
-     * @param $openid
-     * @return array|object|\Overtrue\Http\Support\Collection|\Psr\Http\Message\ResponseInterface|string
-     */
-    public function getUser($appid, $openid)
-    {
-        $params = ['query' => compact('appid', 'openid')];
-        return $this->request('api/oauth/user', $params);
-    }
+	/**
+	 * get user.
+	 *
+	 * @param $appid
+	 * @param $openid
+	 *
+	 * @return array|object|\Overtrue\Http\Support\Collection|\Psr\Http\Message\ResponseInterface|string
+	 */
+	public function getUser($appid, $openid)
+	{
+		$params = ['query' => compact('appid', 'openid')];
+
+		return $this->request('api/oauth/user', $params);
+	}
 
 	/**
 	 * @param $appid
@@ -69,130 +76,133 @@ class Client
 	 * @return array|object|\Overtrue\Http\Support\Collection|\Psr\Http\Message\ResponseInterface|string
 	 * @throws \iBrand\Common\Exceptions\Exception
 	 */
-    public function getFans($appid, $openid)
-    {
-	    $params = ['query' => compact('appid', 'openid')];
+	public function getFans($appid, $openid)
+	{
+		$params = ['query' => compact('appid', 'openid')];
 
-	    return $this->request('api/fans/get', $params, 'POST');
-    }
+		return $this->request('api/fans/get', $params, 'POST');
+	}
 
+	/**
+	 * @param        $appid
+	 * @param        $page
+	 * @param        $width
+	 * @param        $scene
+	 * @param string $type
+	 * @param string $storage
+	 * @param        $uuid
+	 *
+	 * @return mixed
+	 */
+	public function createMiniQrcode($appid, $page, $width, $scene, $type = 'share', $storage = 'public', $uuid)
+	{
 
-    /**
-     * @param $appid
-     * @param $page
-     * @param $width
-     * @param $scene
-     * @param string $type
-     * @param string $storage
-     * @param $uuid
-     * @return mixed
-     */
-    public function createMiniQrcode($appid, $page, $width, $scene, $type = 'share', $storage = 'public', $uuid)
-    {
+		$img_name = $scene . '_' . $type . '_' . $appid . '_mini_qrcode.jpg';
 
-        $img_name = $scene . '_' . $type . '_' . $appid . '_mini_qrcode.jpg';
+		$savePath = $type . '/mini/qrcode/' . $img_name;
 
-        $savePath = $type . '/mini/qrcode/' . $img_name;
+		if (!empty($uuid)) {
 
-        if (!empty($uuid)) {
+			$savePath = $uuid . '/' . $savePath;
+		}
+		$params = [
+			'scene'    => $scene,
+			'optional' => [
+				'page'  => $page,
+				'width' => $width,
+			],
+		];
 
-            $savePath = $uuid . '/' . $savePath;
-        }
-        $params = [
-            'scene' => $scene,
-            'optional' => [
-                'page' => $page,
-                'width' => $width
-            ],
-        ];
+		$new['json'] = $params;
 
-        $new['json'] = $params;
+		$body = $this->request("api/mini/app_code/getUnlimit?appid=$appid&uuid=$uuid", $new, 'POST', true);
 
-        $body = $this->request("api/mini/app_code/getUnlimit?appid=$appid&uuid=$uuid", $new, 'POST', true);
+		Storage::disk($storage)->put($savePath, $body->getBody());
 
-        Storage::disk($storage)->put($savePath, $body->getBody());
+		$result = Storage::disk($storage)->url($savePath);
 
-        $result = Storage::disk($storage)->url($savePath);
+		return $result;
+	}
 
-        return $result;
+	/**
+	 *
+	 * @param $appid
+	 * @param $message
+	 *
+	 * @return array|object|\Overtrue\Http\Support\Collection|\Psr\Http\Message\ResponseInterface|string
+	 */
+	public function sendTemplateMessage($appid, $message)
+	{
+		$data['json'] = $message;
 
-    }
+		return $this->request("api/notice/send?appid=$appid", $data, 'POST');
+	}
 
+	/**
+	 * get js config.
+	 *
+	 * @param $url
+	 * @param $appid
+	 *
+	 * @return array|object|\Overtrue\Http\Support\Collection|\Psr\Http\Message\ResponseInterface|string
+	 */
+	public function getJsConfig($url, $appid)
+	{
+		return $this->request("api/js/config?appid=" . $appid . "&url=" . $url);
+	}
 
-    /**
-     *
-     * @param $appid
-     * @param $message
-     * @return array|object|\Overtrue\Http\Support\Collection|\Psr\Http\Message\ResponseInterface|string
-     */
-    public function sendTemplateMessage($appid, $message)
-    {
-        $data['json'] = $message;
+	/**
+	 * @param        $url
+	 * @param array  $params
+	 * @param string $method
+	 * @param bool   $returnRaw
+	 *
+	 * @return array|object|\Overtrue\Http\Support\Collection|\Psr\Http\Message\ResponseInterface|string
+	 * @throws \Psr\SimpleCache\InvalidArgumentException
+	 * @throws \iBrand\Common\Exceptions\Exception
+	 */
+	protected function request($url, $params = [], $method = 'GET', $returnRaw = false)
+	{
+		$headers = [
+			'headers' => [
+				'Authorization' => 'Bearer ' . $this->getAccessToken()->getToken(),
+			],
+		];
 
-        return $this->request("api/notice/send?appid=$appid", $data, 'POST');
+		$params = array_merge($params, $headers);
 
-    }
+		return $this->getHttpClient()->request($url, $method, $params, $returnRaw);
+	}
 
+	/**
+	 * @return HttpClient
+	 */
+	public function getHttpClient()
+	{
+		$config           = ['base_uri' => $this->baseUri];
+		$http             = HttpClient::create($config);
+		$this->httpClient = $http;
 
-    /**
-     * get js config.
-     * @param $url
-     * @param $appid
-     * @return array|object|\Overtrue\Http\Support\Collection|\Psr\Http\Message\ResponseInterface|string
-     */
-    public function getJsConfig($url, $appid)
-    {
-        return $this->request("api/js/config?appid=" . $appid . "&url=" . $url);
-    }
+		return $this->httpClient;
+	}
 
-    /**
-     * @param $url
-     * @param array $params
-     * @param string $method
-     * @param bool $returnRaw
-     * @return array|object|\Overtrue\Http\Support\Collection|\Psr\Http\Message\ResponseInterface|string
-     * @throws \Psr\SimpleCache\InvalidArgumentException
-     * @throws \iBrand\Common\Exceptions\Exception
-     */
-    protected function request($url, $params = [], $method = 'GET', $returnRaw = false)
-    {
-        $headers = [
-            'headers' => [
-                'Authorization' => 'Bearer ' . $this->getAccessToken()->getToken()
-            ]
-        ];
+	/**
+	 * @return AccessToken
+	 */
+	public function getAccessToken(): AccessToken
+	{
+		return $this->accessToken;
+	}
 
-        $params = array_merge($params, $headers);
+	/**
+	 * @param $accessToken
+	 *
+	 * @return $this
+	 */
+	public function setAccessToken($accessToken)
+	{
+		$this->accessToken = $accessToken;
 
-        return $this->getHttpClient()->request($url, $method, $params, $returnRaw);
-    }
-
-    /**
-     * @return HttpClient
-     */
-    public function getHttpClient()
-    {
-        $config = ['base_uri' => $this->baseUri];
-        $http = HttpClient::create($config);
-        $this->httpClient = $http;
-        return $this->httpClient;
-    }
-
-    /**
-     * @return AccessToken
-     */
-    public function getAccessToken(): AccessToken
-    {
-        return $this->accessToken;
-    }
-
-    /**
-     * @param $accessToken
-     * @return $this
-     */
-    public function setAccessToken($accessToken)
-    {
-        $this->accessToken = $accessToken;
-        return $this;
-    }
+		return $this;
+	}
 }
